3d46f27aa73572b2bc92f0640800223f15697a6f
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-12-16 11:33:48 +0100

    add endpoint for tasks

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ mongoose.connect(mongoUrl, {
 });
 mongoose.Promise = Promise;
 
+const Task = mongoose.model("Task", {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
@@ -25,6 +41,15 @@ app.use(bodyParser.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+// sort makes sure that they are sent back in descending order (des)
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(tasks);
 });
 
 // Start the server
